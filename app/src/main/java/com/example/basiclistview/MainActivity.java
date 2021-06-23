60cba9acf9c1c80155d3d9c178354446978d24e6
vatsks <iamksvats@gmail.com>
2021-06-24 00:45:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/basiclistview/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.basiclistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import static java.util.Arrays.asList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ListView listView=findViewById(R.id.listView);
+        final ArrayList<String> arrayList =new ArrayList<String>(asList ("helo","bye","zehr"));
+        ArrayAdapter<String> arrayAdapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),arrayList.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
